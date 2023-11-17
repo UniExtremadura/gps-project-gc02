fc744d570ff947af831d1350925ce2edd8609d49
Ismael <isbocadul@alumnos.unex.es>
2023-11-17 23:21:15 +0100

    Interfaz de Usuario de la pantalla de Modificar Perfil

---
+++ app/src/main/java/com/example/gc02/view/ModifyProfileActivity.kt
@@ -0,0 +1,12 @@
+package com.example.gc02.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.gc02.R
+
+class ModifyProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_modify_profile)
+    }
+}
