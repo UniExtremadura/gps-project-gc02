d927a939b5284f5d828d46535e9832d503ab7f3d
Ismael <isbocadul@alumnos.unex.es>
2023-11-17 18:25:02 +0100

    Creacion HomeActivity

---
+++ app/src/main/java/com/example/gc02/views/HomeActivity.kt
@@ -0,0 +1,12 @@
+package com.example.gc02.views
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.gc02.R
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
