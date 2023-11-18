82e975c09d4ee7578fdf82fb84c865215f8625b0
sergi <hmsergio4a@gmail.com>
2023-11-18 09:53:40 +0100

    Creacion Activity y Layout de Modificar Articulo

---
+++ app/src/main/java/com/example/gc02/view/ModificarArticuloActivity.kt
@@ -0,0 +1,12 @@
+package com.example.gc02.view
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.gc02.R
+
+class ModificarArticuloActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_modificar_articulo)
+    }
+}
