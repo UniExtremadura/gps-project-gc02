3e6908d944480613417bc9345fd7ddb2229d33e5
Guilledc2409 <gdueasco@alumnos.unex.es>
2023-11-25 18:47:55 +0100

    GC02-77 Captura de información del cerrar sesión 2.

---
+++ app/src/main/java/com/example/gc02/view/home/SettingFragment.kt
@@ -0,0 +1,43 @@
+package com.example.gc02.view.home
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.gc02.R
+import com.example.gc02.databinding.FragmentSettingBinding
+import com.example.gc02.view.LoginActivity
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [SettingFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class SettingFragment : Fragment() {
+
+    private lateinit var binding: FragmentSettingBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment using View Binding
+        binding = FragmentSettingBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.btnLogout.setOnClickListener {
+            val intent = Intent(requireContext(), LoginActivity::class.java)
+            startActivity(intent)
+
+            // Cierra la actividad actual del fragmento si es necesario
+            requireActivity().finish()
+        }
+    }
+
+}
