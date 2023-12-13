a9569440a2f14991627950a9618992b8cd83a96e
Ismael <isbocadul@alumnos.unex.es>
2023-12-13 13:01:57 +0100

    GC02-38 Capturar informacion

---
+++ app/src/main/java/com/example/gc02/model/Article.kt
@@ -0,0 +1,13 @@
+package com.example.gc02.model
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.io.Serializable
+@Entity
+data class Article(
+    @PrimaryKey(autoGenerate = true) var articleId: Long,
+    val title: String = "",
+    val description: String = "",
+    val price: String = ""
+    //var image: String = "" //IMAGEN PATH UNIDO AL API??
+) : Serializable
