1fe7119c3670c7a0f6d91977de2d326615e16c17
jrodrigusx <jrodrigusx@alumnos.unex.es>
2023-12-15 13:14:49 +0100

    GC02-89 Capturar la información del Usuario de realizar compra

---
+++ app/src/main/java/com/example/gc02/view/RealizarCompraActivity.kt
@@ -0,0 +1,69 @@
+package com.example.gc02.view
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContentProviderCompat.requireContext
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.navArgs
+import androidx.navigation.navArgs
+
+import com.example.gc02.database.BaseDatos
+import com.example.gc02.databinding.ActivityRealizarCompraBinding
+import com.example.gc02.model.Article
+import com.example.gc02.view.home.ConsultarDetallesArticuloFragmentArgs
+import kotlinx.coroutines.launch
+
+
+class RealizarCompraActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityRealizarCompraBinding
+    private lateinit var db: BaseDatos
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        db = BaseDatos.getInstance(applicationContext)!!
+
+        //view binding and set content view
+        binding = ActivityRealizarCompraBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //views initialization and listeners
+        setUpUI()
+        setUpListeners()
+
+    }
+
+
+    private fun setUpUI() {
+        //get attributes from xml using binding
+    }
+
+    private fun setUpListeners() {
+        val shop = intent.getSerializableExtra("shop") as Article
+        with(binding) {
+
+            comprarArtculoButton.setOnClickListener {
+                lifecycleScope.launch {
+                    db.articleDao().delete(shop)
+                    navigateToValoracion()
+                }
+
+            }
+            cancelarComprarArticulobutton.setOnClickListener{
+                finish()
+            }
+        }
+    }
+
+    private fun navigateToValoracion() {
+        val shop = intent.getSerializableExtra("shop") as Article
+        val intent = Intent(this, ValoracionActivity::class.java)
+        intent.putExtra("sellerId", "")
+        startActivity(intent)
+    }
+
+
+}
+
+
