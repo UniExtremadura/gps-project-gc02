be7a04a0b157cdfa5d2652f55160ecc3c688e545
Guilledc2409 <gdueasco@alumnos.unex.es>
2023-12-15 13:06:01 +0100

    GC02-94_Validar_Entrada_Usuario

--- app/src/main/java/com/example/gc02/view/ValoracionActivity.kt
+++ app/src/main/java/com/example/gc02/view/ValoracionActivity.kt
@@ -1,11 +1,15 @@
 package com.example.gc02.view
 
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import com.example.gc02.R
 import com.example.gc02.database.BaseDatos
 import com.example.gc02.databinding.ActivityCrearPerfilBinding
 import com.example.gc02.databinding.ActivityValoracionBinding
+import com.example.gc02.databinding.FragmentSettingBinding
 import com.example.gc02.model.Valuation
 
 class ValoracionActivity : AppCompatActivity() {
