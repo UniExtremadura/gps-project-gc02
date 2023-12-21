a37378e2f9910d51ba813bc827fa5232a274d863
jrodrigusx <jrodrigusx@alumnos.unex.es>
2023-12-21 18:13:25 +0100

    GC02-157 Crear Test

--- app/src/androidTest/java/com/example/gc02/view/home/ConsultarComentarioTest.kt
+++ app/src/androidTest/java/com/example/gc02/view/home/ConsultarComentarioTest.kt
@@ -3,11 +3,13 @@ package com.example.gc02.view.home
 import android.content.Context
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.*
 import androidx.test.espresso.assertion.ViewAssertions.*
 import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.espresso.contrib.RecyclerViewActions
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
@@ -16,6 +18,9 @@ import com.example.gc02.database.UserDao
 import com.example.gc02.model.User
 import com.example.gc02.view.LoginActivity
 import com.example.gc02.R
+import com.example.gc02.UnitTests.CommentTest
+import com.example.gc02.database.ComentarioDao
+import com.example.gc02.model.Comentario
 import kotlinx.coroutines.runBlocking
 import org.hamcrest.Description
 import org.hamcrest.Matcher
@@ -29,7 +34,7 @@ import org.junit.runner.RunWith
 
 @LargeTest
 @RunWith(AndroidJUnit4::class)
-class ConsultarPerfilFragmentTest {
+class ConsultarComentarioTest {
 
     @Rule
     @JvmField
@@ -37,31 +42,55 @@ class ConsultarPerfilFragmentTest {
 
     private lateinit var volatileBD: BaseDatos
     private lateinit var userDAO: UserDao
+    private lateinit var comentarioDao: ComentarioDao
 
     @Before
-    fun setUpUser(){
+    fun setUpUser() {
         var context: Context = ApplicationProvider.getApplicationContext<Context>()
         volatileBD = BaseDatos.getInstance(context)!!
 
         userDAO = volatileBD.userDao()
+        comentarioDao = volatileBD.comentarioDao()
 
         //SE CREA UN USER
-        val user = User(99,"user1","user@user.com","user123")
+        val user = User(99, "user1", "user@user.com", "user123")
 
         //SE CREA UN COMENTARIO
+        val comentario1 = Comentario(1,user.name,"Buen vendedor")
+        val comentario2 = Comentario(2,user.name,"Vendedor fiable")
 
 
         //SE INSERTA EN LA BASE DE DATOS
         runBlocking {
             userDAO.insert(user)
+            comentarioDao.insert(comentario1)
+            comentarioDao.insert(comentario2)
+        }
+    }
 
+    private fun childAtPosition(
+        parentMatcher: Matcher<View>, position: Int
+    ): Matcher<View> {
+
+        return object : TypeSafeMatcher<View>() {
+            override fun describeTo(description: Description) {
+                description.appendText("Child at position $position in parent ")
+                parentMatcher.describeTo(description)
+            }
+
+            public override fun matchesSafely(view: View): Boolean {
+                val parent = view.parent
+                return parent is ViewGroup && parentMatcher.matches(parent)
+                        && view == parent.getChildAt(position)
+            }
         }
     }
 
     @Test
-    fun informacionPerfilTest(){
+    fun ConsultarComentarioTest() {
         onView(
-            withId(R.id.loginlayout))
+            withId(R.id.loginlayout)
+        )
             .check(matches(isDisplayed()))
 
         val appCompatEditText = onView(
@@ -126,28 +155,20 @@ class ConsultarPerfilFragmentTest {
             )
         ).perform(click())
 
-// ONVIEW RECICLERVIEW
-// CHECK ONDISPLAY
+        val recyclerView = onView(allOf(
+            withId(R.id.layoutComentarios),
+            isDescendantOfA(withId(R.id.consultarPerfil))
+        ))
+        recyclerView.check(matches(isDisplayed()))
+        recyclerView.perform(RecyclerViewActions.actionOnItem<RecyclerView.ViewHolder>(
+            hasDescendant(withText("Buen vendedor")), click()))
+    }
 
-        private fun childAtPosition(
-            parentMatcher: Matcher<View>, position: Int
-        ): Matcher<View> {
 
-            return object : TypeSafeMatcher<View>() {
-                override fun describeTo(description: Description) {
-                    description.appendText("Child at position $position in parent ")
-                    parentMatcher.describeTo(description)
-                }
 
-                public override fun matchesSafely(view: View): Boolean {
-                    val parent = view.parent
-                    return parent is ViewGroup && parentMatcher.matches(parent)
-                            && view == parent.getChildAt(position)
-                }
-            }
-        }
+
         @After
         fun closeVolatileDB() {
             volatileBD.close()
         }
-    }
+}
